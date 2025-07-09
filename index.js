4a73e88e7091e834336d6bf88d671b16e851aff1
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-09 23:52:09 +0200

    ci

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import Cloudflare from "cloudflare";
 
 try {
-  // Fetch the value of the input 'who-to-greet' specified in action.yml
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-
-  // Record the time of greeting as an output
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  const client = new Cloudflare();
+  const zones = await client.zones.list();
+  console.log(zones);
 } catch (error) {
   // Handle errors and indicate failure
   core.setFailed(error.message);
